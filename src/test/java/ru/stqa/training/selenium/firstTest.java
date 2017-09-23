971bfbdfb05a3bb86f09cb1443c907387af19d11
Dmitry <oddindahouse@gmail.com>
2017-09-23 21:49:58 +0500

    Проверочный коммит

--- src/test/java/ru/stqa/training/selenium/firstTest.java
+++ src/test/java/ru/stqa/training/selenium/firstTest.java
@@ -1,8 +1,6 @@
 package ru.stqa.training.selenium;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
