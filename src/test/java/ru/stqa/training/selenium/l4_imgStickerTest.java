6f1461718c427a0fa7fa153103c4a3efb44e492c
Dmitry <oddindahouse@gmail.com>
2017-10-06 18:48:43 +0500

    Lesson 4 duck Stickers test

---
+++ src/test/java/ru/stqa/training/selenium/l4_imgStickerTest.java
@@ -0,0 +1,24 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class l4_imgStickerTest extends TestBase{
+    @Test
+    public void test1(){
+        driver.navigate().to("http://localhost/litecart/");
+        List<WebElement> liList = driver.findElements(By.cssSelector("li.product"));
+        for (WebElement li : liList) {
+
+            if ( li.findElements(By.cssSelector("div.sticker")).size() == 0){
+                throw new NoSuchElementException();
+            }
+           
+        }
+
+    }
+}
