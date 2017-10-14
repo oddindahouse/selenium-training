15e9ae26a9f9c013fcaeda5dfe276476848b8119
Dmitry <oddindahouse@gmail.com>
2017-10-14 06:03:36 +0500

    L7 home task

---
+++ src/test/java/ru/stqa/training/selenium/L7_CartTest.java
@@ -0,0 +1,69 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import java.util.List;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+public class L7_CartTest extends TestBase {
+
+    @Test
+    public void CartTest(){
+        //add 3 items to cart
+        for (int i = 0; i < 3 ; i++) {
+            driver.get("http://localhost/litecart/");
+            driver.get(driver.findElement(By.cssSelector("div.content li.product a.link")).getAttribute("href"));
+            String oldQuantity = driver.findElement(By.cssSelector("div#cart a.content span.quantity")).getText();
+            //case : REQUIRED FIELDS found
+            for (WebElement element : (driver.findElements(By.cssSelector("div.buy_now [required]")))) {
+                checkUpRequired(element);
+            }
+            //click add button
+            driver.findElement(By.cssSelector("button[name='add_cart_product']")).click();
+            //wait for quantity change
+            wait.until(
+                    d ->  (!oldQuantity.equals(d.findElement(By.cssSelector("div#cart span.quantity")).getText()) )
+            );
+        }
+
+        //going to Cart page
+        driver.findElement(By.cssSelector("div#cart a")).click();
+        //find all li elements with class "item"
+        List<WebElement> liList = (List<WebElement>) ((JavascriptExecutor)driver)
+                .executeScript("return document.querySelectorAll('div#checkout-cart-wrapper li.item')");
+        //find first remove button and click it . repeat until all items gone
+        for (int i = 0; i < liList.size(); i++) {
+            WebElement table = driver.findElement(By.cssSelector("table.dataTable"));
+            driver.findElement(By.cssSelector("div#checkout-cart-wrapper li.item button[name=remove_cart_item]")).click();
+            //wait until table refreshing
+            wait.until(stalenessOf(table));
+            //wait until new table appears. or do not go to if (last element was deleted)
+            if (i < liList.size()-1) {
+                wait.until(d -> d.findElement(By.cssSelector("table.dataTable")));
+            }
+        }
+    }
+
+    private void checkUpRequired(WebElement element){
+        if ( "input".equals(element.getTagName()) ) {
+            if ( "checkbox".equals(element.getAttribute("type")) ){
+                element.click();
+            }
+            //
+            if ( "radio".equals(element.getAttribute("type")) ){
+                element.click();
+            }
+            if ( "text".equals(element.getAttribute("type")) ){
+                element.sendKeys(randomName(5));
+            }
+        }
+        if ( "textarea".equals(element.getTagName()) ){
+            element.sendKeys(randomName(10));
+        }
+        if ( "select".equals(element.getTagName()) ){
+            new Select(element).selectByIndex(1);
+        }
+    }
+}
